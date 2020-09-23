4bdb37348aad7b49e60b6a3c5cd5af7d5938d3a9
Zach Stearns <zach.stearns92@gmail.com>
2020-09-23 09:47:18 -0600

    Modify add ingredient method to add on calories

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,16 +1,23 @@
 class Recipe
-  attr_reader :name, :ingredients_required, :ingredients
+  attr_reader :name,
+              :ingredients_required,
+              :ingredients,
+              :total_calories
 
   def initialize(name)
     @name = name
     @ingredients_required = Hash.new(0)
     @ingredients = []
+    @total_calories = 0
   end
 
   def add_ingredient(ingredient, quantity)
     @ingredients_required[ingredient] += quantity
+
     if !@ingredients.include?(ingredient)
       @ingredients << ingredient
     end
+
+    @total_calories += (ingredient.calories * quantity)
   end
 end
