f67d381179e2157467a89eadb979cacc4a4364e8
Zach Stearns <zach.stearns92@gmail.com>
2020-09-23 10:16:58 -0600

    Add ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredient_list = @recipes.map do |recipe|
+      recipe.ingredients
+    end.flatten
+
+    ingredient_list.map do |ingredient|
+      ingredient.name
+    end.uniq
+  end
 end
