ecba5df35398eaf19e279515fc63d0f6ac1883a1
Zach Stearns <zach.stearns92@gmail.com>
2020-09-23 09:36:41 -0600

    Add existence test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -4,5 +4,8 @@ require "./lib/recipe"
 require "./lib/cook_book"
 
 class CookBookTest < Minitest::Test
-
+  def test_it_exists
+    cookbook = CookBook.new
+    assert_instance_of CookBook, cookbook
+  end
 end
