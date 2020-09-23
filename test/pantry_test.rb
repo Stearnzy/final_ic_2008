52977cbec8d25b4c5587584190c34f94c5088085
Zach Stearns <zach.stearns92@gmail.com>
2020-09-23 09:13:02 -0600

    Add empty pantry test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,4 +7,13 @@ class PantryTest < Minitest::Test
     pantry = Pantry.new
     assert_instance_of Pantry, pantry
   end
+
+  def test_pantry_starts_empty
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    assert_equal ({}), pantry.stock
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
 end
