1d7f0d51442e7373a1029f8759065aefc12994da
Zach Stearns <zach.stearns92@gmail.com>
2020-09-23 09:10:52 -0600

    Add existence test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,8 @@ require "minitest/pride"
 require "./lib/pantry"
 
 class PantryTest < Minitest::Test
-
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 end
