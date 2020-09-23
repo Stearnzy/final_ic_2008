a48f4d55d24ffdc1fc88a870a3d5f48d7639db5c
Zach Stearns <zach.stearns92@gmail.com>
2020-09-23 09:23:05 -0600

    Add readable attribute test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -8,4 +8,13 @@ class RecipeTest < Minitest::Test
     recipe1 = Recipe.new("Mac and Cheese")
     assert_instance_of Recipe, recipe1
   end
+
+  def test_readable_attributes_starting
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_equal "Mac and Cheese", recipe1.name
+    assert_equal ({}), recipe1.ingredients_required
+  end
 end
